580f4359455cceb65c1152dbea6070fa560818b6
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2024-12-14 20:28:56 -0300

    feat: 18 - Finalizando controles da seção

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,41 +1,60 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { UIEvent, useRef, useState } from "react";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
 import { Card, ICardProps } from "../card/Card";
 
 interface ISectionProps {
   title: string;
-  variant: "grid" | "h-list";
   items: ICardProps[];
+  variant: "grid" | "h-list";
 }
-
-export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
+export const Section = ({ title, items, variant = "grid" }: ISectionProps) => {
   const scrollRef = useRef<HTMLUListElement>(null);
 
-  const handleScroll = (scroll: number) => {
-    const currrentScrollRef = scrollRef.current?.scrollLeft || 0;
+  const [scrollAt, setScrollAt] = useState<"start" | "middle" | "end">("start");
+
+  const handleScroll = (event: UIEvent<HTMLUListElement>) => {
+    if (event.currentTarget.scrollLeft === 0) {
+      setScrollAt("start");
+    } else if (
+      event.currentTarget.scrollWidth - event.currentTarget.clientWidth ===
+      event.currentTarget.scrollLeft
+    ) {
+      setScrollAt("end");
+    } else {
+      setScrollAt("middle");
+    }
+  };
+
+  const handleSetScroll = (scroll: number) => {
+    const currentScrollLeft = scrollRef.current?.scrollLeft || 0;
     scrollRef.current?.scrollTo({
       behavior: "smooth",
-      left: currrentScrollRef + scroll,
+      left: currentScrollLeft + scroll,
     });
   };
 
-  useEffect(() => {}, []);
   return (
-    <section className="flex flex-col gap-4 px-4 ">
+    <section className="flex flex-col gap-4 px-4">
       <h2 className="font-bold text-xl">{title}</h2>
 
       <ul
         ref={scrollRef}
         data-variant={variant}
-        className="grid grid-cols-1 sm:grid-cols-none gap-2 data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto"
+        onScroll={handleScroll}
+        className="grid gap-2 grid-cols-1 sm:grid-cols-none data-[variant=grid]:sm:grid-cols-2 data-[variant=grid]:md:grid-cols-3 data-[variant=h-list]:sm:grid-flow-col data-[variant=h-list]:sm:overflow-x-auto"
       >
-        <button
-          onClick={() => handleScroll(-350)}
-          className="bg-primary h-14 w-14 flex items-center justify-center rounded-full sticky my-auto left-0 -ml-14"
-        >
-          <MdKeyboardArrowLeft size={32} />
-        </button>
+        {variant === "h-list" && (
+          <button
+            disabled={scrollAt === "start"}
+            onClick={() => handleSetScroll(-350)}
+            className="h-14 w-14 bg-primary rounded-full hidden sm:flex items-center justify-center sticky my-auto left-0 -ml-14 transition-opacity disabled:opacity-0 active:opacity-80"
+          >
+            <MdKeyboardArrowLeft size={32} />
+          </button>
+        )}
+
         {items.map((item) => (
           <li
             key={item.title}
@@ -43,19 +62,23 @@ export const Section = ({ title, variant = "grid", items }: ISectionProps) => {
             className="w-full data-[variant=h-list]:sm:w-72"
           >
             <Card
-              description={item.description}
               href={item.href}
-              image={item.image}
               title={item.title}
+              image={item.image}
+              description={item.description}
             />
           </li>
         ))}
-        <button
-          onClick={() => handleScroll(350)}
-          className="bg-primary h-14 w-14 flex items-center justify-center rounded-full sticky my-auto right-0 -ml-14"
-        >
-          <MdKeyboardArrowRight size={32} />
-        </button>
+
+        {variant === "h-list" && (
+          <button
+            disabled={scrollAt === "end"}
+            onClick={() => handleSetScroll(350)}
+            className="h-14 w-14 bg-primary rounded-full hidden sm:flex items-center justify-center sticky my-auto right-0 -ml-14 transition-opacity disabled:opacity-0 active:opacity-80"
+          >
+            <MdKeyboardArrowRight size={32} />
+          </button>
+        )}
       </ul>
     </section>
   );
